71d9d920bd786316325c91fb322cad3be46a172a
xoisan <110773590+xoisan@users.noreply.github.com>
2023-01-17 00:42:49 +0700

    Add files via upload

---
+++ gpa.js
@@ -0,0 +1,107 @@
+window.onload = function() {
+  document.getElementById("courseList").style.display = "none";
+}
+
+
+var courseList = [];
+
+function addCourse() {
+
+  var courseName = document.getElementById("courseName").value;
+  var grade = document.getElementById("grade").value;
+  var credits = document.getElementById("credits").value;
+
+  // Validate the input
+  if (!courseName) courseName = courseList.length + 1;
+  if (!grade || !credits) {
+    alert("Please enter grade and credits!");
+    return;
+  }
+  if (credits < 1) {
+    alert("Please enter a valid number of credits greater than 1!");
+    return;
+  }
+  if (!(grade == "A+" || grade == "A" || grade == "B+" || grade == "B" || grade == "C+" || grade == "C" ||grade == "D+" || grade == "D" || grade == "F")) {
+    alert("Invalid grade entered for course " + courseName);
+    return;
+  }
+  var table = document.getElementById("courseList");
+  var row = table.insertRow(-1);
+  var nameCell = row.insertCell(0);
+  nameCell.innerHTML = courseName;
+  var gradeCell = row.insertCell(1);
+  gradeCell.innerHTML = grade;
+  var creditsCell = row.insertCell(2);
+  creditsCell.innerHTML = credits;
+  var deleteCell = row.insertCell(3);
+  var deleteBtn = document.createElement("button");
+  deleteBtn.innerHTML = "X";
+  deleteBtn.onclick = function() {
+    deleteCourse(row);
+  };
+  deleteCell.appendChild(deleteBtn);
+  courseList.push({ name: courseName, grade: grade, credits: credits });
+  document.getElementById("courseList").style.display = "";
+  calculateGPA();
+  document.getElementById("courseName").value = "";
+  document.getElementById("grade").value = "";
+  document.getElementById("credits").value = "";
+}
+
+function calculateGPA() {
+  if(courseList.length == 0) {
+    document.getElementById("gpa").innerHTML = "No courses added";
+    document.getElementById("courseList").style.display = "none";
+    return;
+  }
+  var totalPoints = 0;
+  var totalCredits = 0;
+  for (var i = 0; i < courseList.length; i++) {
+    var course = courseList[i];
+    var credits = course.credits;
+    totalCredits += parseInt(credits);
+    switch (course.grade) {
+      case "A+":
+        totalPoints += 4 * credits;
+        break;
+      case "A":
+        totalPoints += 3.7 * credits;
+        break;
+      case "B+":
+        totalPoints += 3.5 * credits;
+        break;
+      case "B":
+        totalPoints += 3 * credits;
+        break;
+      case "C+":
+        totalPoints += 2.5 * credits;
+        break;
+      case "C":
+        totalPoints += 2 * credits;
+        break;
+      case "D+":
+        totalPoints += 1.5 * credits;
+        break;
+      case "D":
+        totalPoints += 1 * credits;
+        break;
+      case "F":
+        totalPoints += 0;
+        break;
+      default: totalPoints += 0;
+    }
+  }
+  var gpa = totalPoints / totalCredits;
+  document.getElementById("gpa").innerHTML = "GPA: " + gpa.toFixed(2);
+}
+
+  
+function deleteCourse(row) {
+  var table = document.getElementById("courseList");
+  var index = row.rowIndex;
+  table.deleteRow(index);
+  courseList.splice(index-1, 1);
+  calculateGPA();
+}
+
+  
